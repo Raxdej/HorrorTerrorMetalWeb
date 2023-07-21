59876496de60fb378540305f0842a7c463a6c425
Raxdej <d3r4jx@gmail.com>
2023-07-20 20:22:13 -0600

    Deployment Test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Librerías
 import express from 'express';
 import ejs from 'ejs';
+import morgan from 'morgan';
 import {dirname, join} from 'path';
 import { fileURLToPath } from 'url';
 
@@ -26,9 +27,11 @@ const app = express();
 // Importamos la dirección del proyecto y se establece como estática
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
 
 app.use(indexRoutes);
 app.use(express.static(join(__dirname, 'public')));
@@ -36,5 +39,5 @@ app.use(express.static(join(__dirname, 'public')));
 
 
 app.listen(app.get("port"));
-console.log('Server is listening on port ', app.get("port"));
-console.log(join(__dirname, 'views'));
+//console.log('Server is listening on port ', app.get("port"));
+//console.log(join(__dirname, 'views'));
