9d70addb3ee08a7ab8ba301e4e73d0cfdfa2500c
Raxdej <d3r4jx@gmail.com>
2023-07-20 21:15:27 -0600

    Test3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // Librerías
 import express from 'express';
 import ejs from 'ejs';
-import morgan from 'morgan';
 import {dirname, join} from 'path';
 import { fileURLToPath } from 'url';
 
@@ -27,11 +26,9 @@ const app = express();
 // Importamos la dirección del proyecto y se establece como estática
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(morgan('dev'));
 
 app.use(indexRoutes);
 app.use(express.static(join(__dirname, 'public')));
@@ -39,5 +36,5 @@ app.use(express.static(join(__dirname, 'public')));
 
 
 app.listen(app.get("port"));
-//console.log('Server is listening on port ', app.get("port"));
-//console.log(join(__dirname, 'views'));
+console.log('Server is listening on port ', app.get("port"));
+console.log(join(__dirname, 'views'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router } from "express";
 import bodyParser from 'body-parser';
 
 const router = Router();
